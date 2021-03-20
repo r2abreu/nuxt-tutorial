8ef10c583c815c5ff960bdd69d9d99a05e49ca68
Arturo Abreu <arturoabreu@MacBook-Air-de-Administracion-4.local>
2021-03-20 10:47:04 +0100

    Initial commit with Nuxt structure closes #1

---
+++ pages/users/_id.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>This is the page for the sub route id: {{ getId }}</h1>
+</template>
+
+<script>
+export default {
+  name: 'UserId',
+  validate(context) {
+    return !isNaN(context.data.params.id);
+  },
+  computed: {
+    getId() {
+      return this.$route.params.id;
+    },
+  },
+};
+</script>
